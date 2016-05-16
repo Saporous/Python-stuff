b22a88d4eaec49ea068a0ca6dddb323609863e54
Jeff-Laptop.ST3 <jeff2435@gmail.com>
2016-05-16 16:08:08 -0700

    Added string keyword arguments

--- 0-2 Hello World.py
+++ 0-2 Hello World.py
@@ -1,13 +1,33 @@
 # Basic print
 print("Hello World")
 
+# How to get rid of newline
+print("Hello",end='')
+print(" World")
+
+# Example output
+print('cats','dogs','mice')
+# Modify separator
+print('cats','dogs','mice',sep=',')
+
+# Print raw strings
+print(r'That\'s Alice\'s cat!')
+
 # String concatenation
 first_name = "ada"
 last_name = "lovelace"
 full_name = first_name + " " + last_name
 
+# Multiline strings
+print('''Dear Alice,
+
+Your cat has been stolen.
+
+Regards,
+Jeff''')
+
 # title() capitalizes the first letter of words
-print(full_name.title())
+print(full_name.title(),end='')
 
 # \t = tab, \n = newline
 print("\t\n"+first_name)
