16214bae3f7f1d23a98fb1ed4c6f8df9929d3f58
Jeff-Laptop.ST3 <jeff2435@gmail.com>
2016-05-20 17:34:58 -0700

    Added regex verbose

---
+++ Regex.py
@@ -0,0 +1,25 @@
+import re
+
+text = 'Something number 444-444-4444'
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+
+# Search through the text & store in match
+match = phoneNumRegex.search(text)
+print("Phone # is:" + match.group())
+
+phoneParens = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
+match = phoneParens.search(text)
+print(match.group(1))
+print(match.group(2))
+print(match.group(3))
+print(match.group())
+
+phoneRegex = re.compile(r'''(
+	(\d{3}|\(\d{3}\))?		# Area code
+	(\s|-|\.)?				# Separator
+	(\d{3})					# First 3 numbers
+	(\s|-|\.)?				# Separator
+	(\d{4})					# Last 4 numbers
+	)''', re.X)
+match = phoneRegex.search(text)
+print(match.group())
