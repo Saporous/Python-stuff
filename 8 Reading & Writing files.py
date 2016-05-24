750ec15122875c2be06bdfbbe163ed156dbeee9e
Jeff-Laptop.ST3 <jeff2435@gmail.com>
2016-05-23 18:27:59 -0700

    WIP reading/writing directories

---
+++ 8 Reading & Writing files.py
@@ -0,0 +1,38 @@
+import os
+# https://docs.python.org/3/library/os.path.html
+
+cwd = os.getcwd()
+print('cwd:',cwd)
+print("getcwd == abspath('.'):", cwd == os.path.abspath('.'))
+print("getcwd == isabs()",os.path.isabs(cwd))
+
+#os.makedirs(os.path.join(cwd, 'test'))
+os.makedirs(os.path.join(cwd, 'test', 'lala'))
+# Note makedirs creates all necessary directories
+print('Test/lala directories created!')
+
+print('.\\test exists?:',os.path.exists(os.path.join(cwd, 'test')))
+print('.\\test\\lala exists?:',os.path.exists(os.path.join(cwd, 'test','lala')))
+print('.\\test\\foo exists?:',os.path.exists(os.path.join(cwd, 'test','foo')))
+
+file = open(os.path.join(cwd,'test','bar.txt'),'a')
+file.write('Hello World!\n')
+file.close()
+
+os.remove(os.path.join(cwd,'test','bar.txt'))
+print('Test\\bar.txt deleted!')
+
+os.rmdir(os.path.join(cwd, 'test', 'lala'))
+os.rmdir(os.path.join(cwd, 'test'))
+print('Test\\lala directories deleted!')
+
+path = 'C:\\Windows\\System32\\calc.exe'
+print('Example path:',path)
+print('basename:',os.path.basename(path))
+print('dirname:',os.path.dirname(path))
+print('split:',os.path.split(path))
+
+print('path.split(os.path.sep):',path.split(os.path.sep))
+
+
+
